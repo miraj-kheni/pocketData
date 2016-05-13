9449e595b5eebd97b404c7c8df1555db2fb8ef5a
miraj-kheni <mirajkheni@ymail.com>
2016-05-13 16:59:49 -0400

    reading queries from JSON

--- SqliteTrace/app/src/main/java/com/example/SqliteTrace/MainActivity.java
+++ SqliteTrace/app/src/main/java/com/example/SqliteTrace/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.SqliteTrace;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.List;
@@ -11,10 +17,12 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
+import android.net.LocalSocketAddress;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 
 public class MainActivity extends Activity {
 
@@ -35,6 +43,17 @@ public class MainActivity extends Activity {
         putMarker("1", "events/power/cpu_frequency/enable");
         putMarker("1", "tracing_on");
 */
+
+        Bundle extras = this.getIntent().getExtras();
+        boolean flushCacheEnable = false;
+        if (extras != null) {
+            if(extras.containsKey("flushCache")) {
+                flushCacheEnable = extras.getBoolean("flushCache");
+            }
+        }
+
+
+
         putMarker("START: App started\n", "trace_marker");
 
         putMarker("\"EVENT\":\"DATABASE_OPEN_START\"}\n","trace_marker");
@@ -43,26 +62,26 @@ public class MainActivity extends Activity {
         putMarker("\"EVENT\":\"DATABASE_OPEN_END\"}\n","trace_marker");
 
         boolean isTableExists = false;
-        Cursor cursor1 = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'contacts'",null);
+        Cursor cursor1 = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'employee'",null);
 
         if(cursor1.moveToFirst()) {
             isTableExists = true;
         }
 
         if(!isTableExists) {
+
             putMarker("{\"EVENT\":\"CREATE_START\"}", "trace_marker");
-            db.execSQL("CREATE TABLE contacts (id INTEGER PRIMARY KEY, name TEXT, number TEXT)");
+            db.execSQL("CREATE TABLE dept (id INTEGER PRIMARY KEY, name TEXT) ");
             putMarker("{\"EVENT\":\"CREATE_END\"}", "trace_marker");
 
-            String sql = "INSERT INTO contacts VALUES(?,?,?)";
+            String sql = "INSERT INTO dept VALUES(?,?)";
             putMarker("{\"EVENT\":\"TRANSACTION_START\"}", "trace_marker");
             db.beginTransaction();
             SQLiteStatement stmt = db.compileStatement(sql);
-            for (int i = 0; i < 500; i++) {
+            for (int i = 1; i < 11; i++) {
                 putMarker("{\"EVENT\":\"INSERT_START\"}", "trace_marker");
-                stmt.bindLong(1,i);
-                stmt.bindString(2, "John Doe");
-                stmt.bindString(3,"9797997922");
+                stmt.bindLong(1, i);
+                stmt.bindString(2, "Dept"+i);
                 stmt.execute();
                 stmt.clearBindings();
                 //db.execSQL("INSERT INTO contacts VALUES(" + i + ",'John Doe','9898889889')");
@@ -71,6 +90,86 @@ public class MainActivity extends Activity {
             db.setTransactionSuccessful();
             db.endTransaction();
             putMarker("{\"EVENT\":\"TRANSACTION_END\"}", "trace_marker");
+
+
+            putMarker("{\"EVENT\":\"CREATE_START\"}", "trace_marker");
+            db.execSQL("CREATE TABLE employee (id INTEGER PRIMARY KEY, name TEXT, dept INTEGER, designation TEXT, FOREIGN KEY(dept) REFERENCES dept(id))");
+            putMarker("{\"EVENT\":\"CREATE_END\"}", "trace_marker");
+
+            Random random = new Random();
+            sql = "INSERT INTO employee VALUES(?,?,?,?)";
+            putMarker("{\"EVENT\":\"TRANSACTION_START\"}", "trace_marker");
+            db.beginTransaction();
+            stmt = db.compileStatement(sql);
+            for (int i = 0; i < 500; i++) {
+                putMarker("{\"EVENT\":\"INSERT_START\"}", "trace_marker");
+                stmt.bindLong(1, i);
+                stmt.bindString(2, "John Doe"+random.nextInt() % 20);
+                stmt.bindLong(3, random.nextInt() % 10 + 1);
+                stmt.bindString(4,"Designation");
+                stmt.execute();
+                stmt.clearBindings();
+                //db.execSQL("INSERT INTO contacts VALUES(" + i + ",'John Doe','9898889889')");
+                putMarker("{\"EVENT\":\"INSERT_END\"}", "trace_marker");
+            }
+            db.setTransactionSuccessful();
+            db.endTransaction();
+            putMarker("{\"EVENT\":\"TRANSACTION_END\"}", "trace_marker");
+
+            putMarker("{\"EVENT\":\"CLOSE_START\"}\n", "trace_marker");
+            db.close();
+            putMarker("{\"EVENT\":\"CLOSE_END\"}\n", "trace_marker");
+            putMarker("END: app finished\n", "trace_marker");
+
+            this.finishAffinity();
+            return;
+
+        }
+
+        String type = "";
+        String query = "";
+        InputStream queryFile;
+        try {
+            queryFile = this.openFileInput("query.log");
+
+            JsonReader reader = new JsonReader(new InputStreamReader(queryFile,"UTF-8"));
+            reader.beginArray();
+            while(reader.hasNext()) {
+                reader.beginObject();
+                while (reader.hasNext()) {
+                    String temp = reader.nextName();
+                    if (temp.equals("type")) {
+                        type = reader.nextString();
+                    } else if (temp.equals("query")) {
+                        query = reader.nextString();
+                    }
+                }
+                reader.endObject();
+                //flushCache();
+                if(type.equals("SELECT")) {
+                    putMarker("{\"EVENT\":\"SELECT_START\"}\n","trace_marker");
+                    Cursor cursor = db.rawQuery(query,null);
+                    if(cursor.moveToFirst()) {
+                        int numColumns = cursor.getColumnCount();
+                        do {
+                            for(int j=0; j< numColumns; j++) {
+                                //String temp = cursor.toString();
+                            }
+                            //process cursor
+                        } while(cursor.moveToNext());
+                    }
+                    cursor.close();
+                    putMarker("{\"EVENT\":\"SELECT_END\"}\n", "trace_marker");
+                } else if(type.equals("INSERT") || type.equals("UPDATE") || type.equals("DELETE")) {
+                    putMarker("{\"EVENT\":\""+type+"_START\"}\n","trace_marker");
+                    db.execSQL(query);
+                    putMarker("{\"EVENT\":\""+type+"_END\"}\n","trace_marker");
+                }
+            }
+            reader.endArray();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
         if(db.isOpen()) {
@@ -79,12 +178,13 @@ public class MainActivity extends Activity {
             putMarker("\"EVENT\":\"DATABASE_CLOSE_END\"}\n","trace_marker");
         }
 
+        /*
         //putMarker("\"EVENT\":\"DATABASE_OPEN_START\"}\n","trace_marker");
         db = SQLiteDatabase.openDatabase(this.getDatabasePath("Contacts").getPath(), null, SQLiteDatabase.OPEN_READONLY);
         //putMarker("\"EVENT\":\"DATABASE_OPEN_END\"}\n","trace_marker");
 
         putMarker("{\"EVENT\":\"SELECT_START\"}\n","trace_marker");
-        Cursor cursor = db.rawQuery("SELECT * FROM contacts", null);
+        Cursor cursor = db.rawQuery("SELECT * FROM contacts;", null);
         if (cursor.moveToFirst()) {
             do {
                 int id = Integer.parseInt(cursor.getString(0));
@@ -93,9 +193,12 @@ public class MainActivity extends Activity {
             } while (cursor.moveToNext());
         }
         putMarker("{\"EVENT\":\"SELECT_END\"}\n","trace_marker");
+
         putMarker("{\"EVENT\":\"CLOSE_START\"}\n", "trace_marker");
         db.close();
         putMarker("{\"EVENT\":\"CLOSE_END\"}\n","trace_marker");
+*/
+
         putMarker("END: app finished\n", "trace_marker");
     /*    putMarker("0", "events/kmem/kmalloc/enable");
         putMarker("0", "events/kmem/kfree/enable");
@@ -133,16 +236,23 @@ public class MainActivity extends Activity {
                 outStream.close();
         }
     }
-/*
-    public static void echoFunc(String val, String filename) {
+
+    public static void flushCache() {
+
         try{
-            Process echoProc = Runtime.getRuntime().exec("sh -c echo " + val + " > " + "/sys/kernel/debug/tracing/");
-            echoProc.waitFor();
+            String[] command = {"/system/xbin/su","-c", "echo 3 > /proc/sys/vm/drop_caches"};
+            Process su = Runtime.getRuntime().exec(command);
+            //DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+            //outputStream.writeBytes("echo 3 > /proc/sys/vm/drop_caches\n");
+            //outputStream.flush();
+            //outputStream.writeBytes("exit\n");
+            //outputStream.flush();
+            su.waitFor();
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
             throw  new RuntimeException(e);
         }
 
-    }*/
+    }
 }
